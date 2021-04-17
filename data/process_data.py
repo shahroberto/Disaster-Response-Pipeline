a9ba4e683e5bd9a790ea3c94bbf43ca00fc79819
shahroberto <rshaheen55@gmail.com>
2021-04-17 02:29:52 +0000

    Fixed if_exists = 'replace' for loading DB error

--- data/process_data.py
+++ data/process_data.py
@@ -51,7 +51,7 @@ def clean_data(df):
 def save_data(df, database_filename):
     """Saves the dataframe df to the database specified by database_filename."""
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('DisasterResponse', engine, index=False)
+    df.to_sql('DisasterResponse', engine, index=False, if_exists='replace')
 
 
 def main():
